ca6b86c52dc24bacfd73dd933629baa2c80660f2
GHarris97 <77605226+GHarris97@users.noreply.github.com>
2021-03-28 23:28:58 -0700

    Add files via upload

---
+++ big/contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <title>Web Developer Examples and Resources by Garcia Harris</title>
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" href="css/big.css" />
+  <link rel="stylesheet" href="css/nav.css"/>
+	<!-- jquerry -->
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<!-- MenuMaker Plugin -->
+	<script src="https://s3.amazonaws.com/menumaker/menumaker.min.js"></script>
+	<!-- Icon Library -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<script src="js/script.js"></script>
+</head>
+ <body> 
+     <header>
+     	<h1> Web Developer Examples and Resources by Garcia Harris</h1>
+     	<nav>
+   		<nav id="cssmenu">
+		<ul>
+		 <li><a href="../index.html"><i class="fa fa-fw fa-institution"></i> WEB161</a></li>
+		 <li><a href="index.html"><i class="fa fa-fw fa-home"></i> BIG Website</a></li>
+		 <li><a href="gallery.html"><i class="fa fa-fw fa-camera-retro"></i> Lightbox II Gallery</a></li> 
+		 <!-- drop down menu for research topics pages --> 
+		 <li><a href="#">Research Topics </a>
+			<ul>
+				<li><a href="smo.html"><i class="fa fa-fw fa-thumbs-o-up"></i> SMO</a></li>
+				<li><a href="accessibility.html"><i class="fa fa-fw fa-globe"></i> Accessibility</a></li>
+				<li><a href="forms.html"><i class="fa fa-fw fa-globe"></i> Forms</a></li>
+			</ul>
+		 </li>
+			<!-- drop down menu for google tools -->
+			<li><a href="#">Google Tools </a>
+			<ul>
+			   <li><a href="seo.html"><i class="fa fa-fw fa-desktop"></i> SEO</a></li>
+			   <li><a href="calendar.html"><i class="fa fa-fw fa-calendar"></i> Calendar</a></li>
+			   <li><a href="map.html"><i class="fa fa-fw fa-map-o"></i> Map</a></li>
+			</ul>
+		 </li>
+		 <li><a href="contact.php"><i class="fa fa-fw fa-server"></i> Contact Garcia</a></li>
+	  </ul>
+</nav>	
+     	</nav>
+     </header>
+     
+  	<main>
+	 <!-- START LEFT COLUMN -->
+	<section class="fullwidth">
+		<h2>Contact Garcia</h2> 
+		 <p>watch supporting viseo to create contents for page</p>
+		<div class="embed-container">
+		<iframe src=""></iframe>		
+	</div>
+	</section>
+	<!-- END LEFT COLUMN -->
+     <footer>
+      <p><small>© 2021 by <a href="contact.php">Garcia Harris</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+	 </main>
+         
+ 
+ </body>
+</html>
